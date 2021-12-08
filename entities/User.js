da604f85b5ce4fab5b7b2865031cccdc15ac8a77
xPiLoYx <rexs.llemel333@gmail.com>
2021-12-08 13:32:46 +0800

    Update User.js

--- entities/User.js
+++ entities/User.js
@@ -1,19 +1,29 @@
-const EntitySchema = require('typeorm').EntitySchema
+const EntitySchema = require('typeorm').EntitySchema //<== to use EntitySchema
 
+//to export our module as EnitytySchema
 module.exports = new EntitySchema({
-    name: "Users",
+    name: "Users", //module name
+    //our columns/object format for our database
     columns: {
         id: {
             primary: true,
             type: "int",
-            generated: true
+            generated: true //to automatically generate id number every successful registration
         },
         username: {
             type: "varchar"
         },
         password:{
             type: "varchar"
-        }
+        },
+        firstname:{
+            type: "varchar"
+        },
+        lastname:{
+            type: "varchar"
+        },
+        birthdate:{
+            type: "varchar"
+        },
     }
-
 })
